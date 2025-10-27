208c6af8115868ecc6b858429b0fc80dea16f6b7
schoolaccGit <maerten@wisc.edu>
2025-10-26 20:07:08 -0500

    Added some Map Screen Functionality

--- client/screens/MapsScreen.js
+++ client/screens/MapsScreen.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location'
 
 export default function MapsScreen() {
+  const [location, setLocation] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() =>{
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted'){
+        setErrorMessage("Permission to access location was denied. You must allow access to use the app.")
+        Alert.alert("Permission denied, you must allow location services to use the app")
+      }
+      let loc = await Location.getCurrentPositionAsync({})
+      setLocation(loc.coords)
+    })()
+  },[])
+
+
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.text}>Maps Screen</Text>
+      <View style={styles.titleContainer}>
+      <Text style={styles.text}>
+        Maps
+      </Text>
+    </View>
+      {/* <Text style={styles.text}>Maps Screen</Text> */}
+      {location && (
+        <MapView
+        style = {styles.map}
+        provider = {MapView.PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}>
+          <Marker
+            coordinate={{latitude:location.latitude, longitude:location.longitude}}
+            title = {"Current Marker"}
+            description={"Current Location"}
+            />
+      </MapView>
+      )}
     </View>
   );
 }
@@ -12,13 +53,19 @@ export default function MapsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  titleContainer:{
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#0B0D17',
+    paddingVertical: 20,
+    backgroundColor: "#0B0D17"
   },
   text: {
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
+    paddingTop:40,
+  },
+  map:{
+    flex: 1, 
   },
 });
