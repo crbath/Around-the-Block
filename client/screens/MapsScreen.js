9d238dd818ba29fc4f1c460c3f96b0fc09702b89
schoolaccGit <maerten@wisc.edu>
2025-11-16 20:29:48 -0600

    Adding the OSM api

--- client/screens/MapsScreen.js
+++ client/screens/MapsScreen.js
@@ -1,12 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import { View, Text, StyleSheet, Alert,Modal,TouchableWithoutFeedback } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import * as Location from 'expo-location'
+import axios from 'axios'
 
 export default function MapsScreen() {
+  //user's location
   const [location, setLocation] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
 
+  //bars from OSM
+  const [bars, setBars] = useState([])
+
+  const [selectedBar, setSelectedBar] = useState(null)
+  const [showModal, setShowModal] = useState(false)
+
+  //using OSM to fetch bars instead of googleplaces
+  useEffect(()=>{
+    if (!location) return;
+
+    const fetchBars = async () =>{
+      try {
+        const lat = location.latitude
+        const long = location.longitude
+        const delta = 0.05 
+
+        const overpassQuery = `
+        [out:json];
+        node
+        ["amenity"="bar"]
+        (${lat-delta},${long-delta},${lat+delta},${long+delta});
+        out;
+        `;
+
+        const response = await axios.post(
+          'https://overpass-api.de/api/interpreter',
+          overpassQuery,
+          {headers:
+            {'Content-Type': 'text/plain'}
+          }
+        )
+        //map each element
+        const nodes = response.data.elements.map (element => ({
+          id: element.id,
+          latitude: element.lat,
+          longitude: element.lon,
+          name: element.tags?.name || 'Unable to retrieve name'
+        }))
+        //save each bar
+        setBars(nodes)
+
+
+      }catch(err){
+        setErrorMessage("Error grabbing bars")
+      }
+
+
+    }
+    fetchBars()
+  },[location])
+
+  //Grab permission for location from user if not already granted
   useEffect(() =>{
     (async () => {
       let {status} = await Location.requestForegroundPermissionsAsync()
@@ -44,8 +98,40 @@ export default function MapsScreen() {
             title = {"Your Location"}
             description={location.latitude + ", " + location.longitude}
             />
+            {bars.map(bar => (
+              <Marker
+              key = {bar.id}
+              coordinate={{latitude: bar.latitude, longitude: bar.longitude}}
+              // title = {bar.name}
+              pinColor = "purple"
+              onPress = {() => {setSelectedBar(bar); setShowModal(true)}}
+              />
+            ))}
       </MapView>
+      
       )}
+      <Modal
+        visible = {showModal}
+        transparent = {true}
+        animationType="fade"
+        onRequestClose={() => setShowModal(false)}>
+          <TouchableWithoutFeedback onPress={()=> setShowModal(false)}>
+        <View style ={styles.modalBackground}>
+          <View style = {styles.modalContainer}>
+
+            <View style={{flex:1, justifyContent:'flex-start', alignItems:'center'}}>
+            <Text style={styles.modalTitle}>{selectedBar?.name}</Text>
+            {/* add other bar information here... and onclick to text to go to bar screen */}
+            </View>
+
+            <Text style={{marginBottom:10, color:'purple'}}
+            onPress={()=> setShowModal(false)}>
+              Close
+            </Text>
+          </View>
+        </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   );
 }
@@ -68,4 +154,26 @@ const styles = StyleSheet.create({
   map:{
     flex: 1, 
   },
+  modalBackground:{
+    flex: 1,
+    backgroundColor:'rgba(0,0,0,0.5)',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  modalContainer: {
+    width:'80%',
+    height:'70%',
+    padding: 20,
+    backgroundColor:'rgba(213, 213, 213, 0.8)',
+    borderRadius: 10,
+    alignItems:'center',
+    justifyContent:'space-between'
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign:'center',
+    color:'rgba(59, 4, 83, 1)'
+  }
 });
